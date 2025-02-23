d6023d194412231070f0b0f5267732715b6dc747
Nilesh <nilesh.jha@onequince.com>
2025-02-24 01:24:17 +0530

    feat: add icons and manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/organisms/Header';
-// import { ThemeScript } from '@/components/ThemeScript';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,6 +17,31 @@ export const metadata: Metadata = {
     'React',
     'Next.js',
   ],
+  icons: {
+    icon: [
+      { url: '/icons/favicon.ico' },
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      {
+        url: '/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: '/icons/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
   authors: [{ name: 'Nilesh' }],
   openGraph: {
     title: 'Nilesh - Software Engineer & Web Developer',
@@ -95,7 +119,6 @@ export default function RootLayout({
             {children}
             <footer></footer>
           </main>
-          {/* <ThemeScript /> */}
         </ThemeProvider>
       </body>
     </html>
