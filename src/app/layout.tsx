5db683f6561747bed1a8b61f80e043f7cec6275d
Nilesh <nilesh.jha@onequince.com>
2025-02-25 23:22:48 +0530

    chore: standardise site icons path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,23 +20,23 @@ export const metadata: Metadata = {
   ],
   icons: {
     icon: [
-      { url: '/icons/favicon.ico' },
-      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icons/site/favicon.ico' },
+      { url: '/icons/site/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icons/site/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       {
-        url: '/icons/android-chrome-192x192.png',
+        url: '/icons/site/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        url: '/icons/android-chrome-512x512.png',
+        url: '/icons/site/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
     ],
     apple: [
       {
-        url: '/icons/apple-touch-icon.png',
+        url: '/icons/site/apple-touch-icon.png',
         sizes: '180x180',
         type: 'image/png',
       },
